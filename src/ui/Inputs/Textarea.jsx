bf590c10796c52d3ba4b47ec3212197bc8f309d6
mostafataher-1911 <mostafataher339@gmail.com>
2025-07-08 17:29:11 +0200

    inputs
    feat(inputs): add input, textarea, icon input, and calendar input components

---
+++ src/ui/Inputs/Textarea.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Textarea({placeholder}) {
+  return (
+    <div>
+      <textarea className="textarea" placeholder={placeholder} ></textarea>
+    </div>
+  )
+}
