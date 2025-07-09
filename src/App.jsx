9d39668a194df4ce97f14c19dbb9bcd50d23e40d
mostafataher-1911 <mostafataher339@gmail.com>
2025-07-09 17:45:12 +0200

    update
    add dev inputs and install react-day-picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import Icons from "./ui/Icons/Icons";
-import CalendarInput from "./ui/Inputs/CalendarInput";
-import IconPlaceholderInput from "./ui/Inputs/Iconplaceholderinput";
-import Inputs from "./ui/Inputs/Inputs";
-import Textarea from "./ui/Inputs/Textarea";
+
 
 
 export default function App() {
@@ -12,7 +8,7 @@ export default function App() {
       <h1 style={{ fontWeight: 900 }}>hello</h1>
       <div style={{ color: "var(--primary-color)",fontWeight:500 }}>
         This uses --color-primary
-        
+     
       </div>
     </>
   );
