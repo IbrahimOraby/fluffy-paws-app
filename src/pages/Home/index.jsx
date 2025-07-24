3b20c865dcb1cf60a473839feefa9a902dec2c8f
Shahenda Elshayal <shahendaelshayal@Shahendas-MacBook-Pro.local>
2025-07-25 02:43:04 +0300

    adding pet form ui

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import PetForm from "../../ui/Pets/PetForm";
 
 const Home = () => {
-  return <div>Home</div>;
+  // return <div>Home</div>;
+  return <PetForm />;
 };
 
 export default Home;
