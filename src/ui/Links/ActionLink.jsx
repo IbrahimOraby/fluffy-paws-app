fb2c50c2bbc9cf12fc52184e38c12b3d2fe10522
IbrahimOraby <orabyibrahim19@gmail.com>
2025-07-25 03:11:19 +0300

    fix: remove min-h and min-w from ActionLink

--- src/ui/Links/ActionLink.jsx
+++ src/ui/Links/ActionLink.jsx
@@ -4,7 +4,7 @@ function ActionLink({ children, path,className = "",...props }) {
   return (
     <Link
       to={path}
-      className={`text-[#BE5985] font-normal p-0 min-h-[3rem] min-w-[3rem] hover:text-[#dfa4ba] ${className}`}
+      className={`text-[#BE5985] font-normal p-0  hover:text-[#dfa4ba] ${className}`}
       {...props}
     >
       {children}
