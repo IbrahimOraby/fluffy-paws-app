a1f99b1fe74f3ce724d36ea7ef0f1b1cd0747b85
IbrahimOraby <orabyibrahim19@gmail.com>
2025-07-29 03:52:31 +0300

    feat: add custom Formik checkbox input

---
+++ src/ui/Inputs/MyCheckboxInput.jsx
@@ -0,0 +1,27 @@
+import { useField } from "formik";
+import { StaticCheckIcon } from "../Icons/StaticIcons";
+
+export const MyCheckboxInput = ({ label, ...props }) => {
+  const [field] = useField({ ...props, type: "checkbox" });
+
+  const isChecked = Array.isArray(field.value)
+    ? field.value.includes(props.value)
+    : field.checked;
+
+  return (
+    <label
+      className={`btn btn-lg cursor-pointer flex items-center gap-2 border transition
+        ${
+          isChecked
+            ? "bg-primary-color text-primary-color-100 border-primary-color"
+            : ""
+        }
+      `}
+      aria-label={label}
+    >
+      <input type="checkbox" {...field} {...props} className="hidden" />
+      <span>{label}</span>
+      {isChecked && <StaticCheckIcon size={18} />}
+    </label>
+  );
+};
