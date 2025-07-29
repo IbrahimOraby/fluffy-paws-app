14c78af89835bc045022b6b705cd9209d9bc674d
Shahenda Elshayal <shahendaelshayal@Shahendas-MacBook-Pro.local>
2025-07-29 23:01:33 +0300

    style: update the color of subheading in message-item compontent

--- src/pages/Profile/components/MessageItem.jsx
+++ src/pages/Profile/components/MessageItem.jsx
@@ -5,8 +5,8 @@ export default function MessageItem({sender,messageSnippet,timestamp}) {
   return (
     <>
       <li className="p-3 bg-light-color border-l-2 transition-all duration-300 ease-in-out hover:bg-light-color-300  border-primary-color rounded-lg flex justify-between items-center">
-        <span className="flex gap-2 items-center ">
-          <SubHeading className="text-subheader-md">{sender}:</SubHeading>{" "}
+        <span className="flex gap-2 items-center text-header-color">
+          <SubHeading className="text-subheader-md ">{sender}:</SubHeading>{" "}
           "{messageSnippet}"
         </span>
         <span className="text-sm text-paragraph-color">{timestamp}</span>
