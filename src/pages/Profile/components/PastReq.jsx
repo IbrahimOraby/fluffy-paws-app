f44737b75be9b460f8a3200b91be43c3a405ec03
Shahenda Elshayal <shahendaelshayal@Shahendas-MacBook-Pro.local>
2025-07-30 01:33:13 +0300

    refactor: update Past-req component with paragraph component

--- src/pages/Profile/components/PastReq.jsx
+++ src/pages/Profile/components/PastReq.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import Paragraph from "../../../ui/Typography/Paragraph/Paragraph";
 
-export default function PastReq({booking}) {
-    return (
-        <div className="p-4 border rounded-lg bg-gray-100">
-          <p className="font-medium">
-            Pet: {booking.name} | Owner: {booking.owner}
-          </p>
-          <p className="text-sm text-gray-600">Date: {booking.date}</p>
-        </div>
-      );
+export default function PastReq({ booking }) {
+  return (
+    <div className="p-4 rounded-lg bg-gray-100">
+      <Paragraph className="text-paragraph-md">
+        <span className="text-paragraph-color">Pet: </span>
+        {booking.name}
+      </Paragraph>
+      <Paragraph className="text-paragraph-md">
+        <span className="text-paragraph-color">Owner: </span>
+        {booking.owner}
+      </Paragraph>
+      <Paragraph className="text-paragraph-xs text-paragraph-color mt-4">
+        Date: {booking.date}
+      </Paragraph>
+    </div>
+  );
 }
