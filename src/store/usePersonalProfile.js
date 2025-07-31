8094a2dadd7db824671d2d48a22df78ef37374a1
IbrahimOraby <orabyibrahim19@gmail.com>
2025-07-31 11:29:44 +0300

    feat: allow the store to persist data in the local storage

--- src/store/usePersonalProfile.js
+++ src/store/usePersonalProfile.js
@@ -1,104 +1,98 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const usePersonalFormStore = create((set) => ({
-  //   personalFormData: {
-  //     // intro: null, // Desktop 7: Steps overview
-  //     contact: null, // Desktop 8: Address and phone number
-  //     availabilityFrequency: null, // Desktop 9: How often do you want to look after pets?
-  //     // availabilityDays: null, // Desktop 10: What days are you available?
-  //     petPreferences: null, // Desktop 11: What pets can you look after?
-  //     experience: null,
-  //     homeType: null, // Desktop 12: Experience and home type
-
-  //     householdInfo: null, // Desktop 13: Do you have kids at home? + age group
-  //     profilePicture: null, // Desktop 16: Upload your profile picture
-  //     // finalTouch: null, // Desktop 15: Finish Your Profile (pre-submission screen)
-  //     idUpload: null, // Desktop 17: Upload your ID
-  //     aboutMe: null // Desktop 18: Tell us a bit about yourself
-  //   },
-
-  //states
-  personalFormData: {
-    contact: null, //done
-    availabilityFrequency: null, //done
-    petPreferences: null, //done
-    experience: null, //done
-    homeType: null,
-    householdInfo: null,
-    profileSetup: null, //in progress
-    aboutMe: null
-  },
-
-  completedForms: {
-    contact: false,
-    availabilityFrequency: false,
-    petPreferences: false,
-    experience: false,
-    homeType: false,
-    householdInfo: false,
-    profileSetup: false,
-    aboutMe: false
-  },
-
-  currentFormIndex: 0,
-
-  //actions
-  setContact: (data) =>
-    set((state) => ({
-      personalFormData: { ...state.personalFormData, contact: data },
-      completedForms: { ...state.completedForms, contact: true }
-    })),
-
-  setAvailabilityFrequency: (data) => {
-    set((state) => ({
+const usePersonalFormStore = create(
+  persist(
+    (set) => ({
+      //states
       personalFormData: {
-        ...state.personalFormData,
-        availabilityFrequency: data
+        contact: null,
+        availabilityFrequency: null,
+        petPreferences: null,
+        experience: null,
+        homeInfo: null,
+        profileSetup: null,
+        aboutMe: null
       },
-      completedForms: { ...state.personalFormData, availabilityFrequency: true }
-    }));
-  },
 
-  setPetPreferences: (data) =>
-    set((state) => ({
-      personalFormData: { ...state.personalFormData, petPreferences: data },
-      completedForms: { ...state.completedForms, petPreferences: true }
-    })),
+      completedForms: {
+        contact: false,
+        availabilityFrequency: false,
+        petPreferences: false,
+        experience: false,
+        homeType: false,
+        householdInfo: false,
+        profileSetup: false,
+        aboutMe: false
+      },
 
-  setExperience: (data) =>
-    set((state) => ({
-      personalFormData: { ...state.personalFormData, experience: data },
-      completedForms: { ...state.completedForms, experience: true }
-    })),
+      currentFormIndex: 0,
 
-  setHomeType: (data) =>
-    set((state) => ({
-      personalFormData: { ...state.personalFormData, homeType: data },
-      completedForms: { ...state.completedForms, homeType: true }
-    })),
+      //actions
+      setContact: (data) =>
+        set((state) => ({
+          personalFormData: { ...state.personalFormData, contact: data },
+          completedForms: { ...state.completedForms, contact: true }
+        })),
 
-  setHouseholdInfo: (data) =>
-    set((state) => ({
-      personalFormData: { ...state.personalFormData, householdInfo: data },
-      completedForms: { ...state.completedForms, householdInfo: true }
-    })),
+      setAvailabilityFrequency: (data) => {
+        set((state) => ({
+          personalFormData: {
+            ...state.personalFormData,
+            availabilityFrequency: data
+          },
+          completedForms: {
+            ...state.personalFormData,
+            availabilityFrequency: true
+          }
+        }));
+      },
 
-  setProfileSetup: (data) =>
-    set((state) => ({
-      personalFormData: { ...state.personalFormData, profileSetup: data },
-      completedForms: { ...state.completedForms, profileSetup: true }
-    })),
+      setPetPreferences: (data) =>
+        set((state) => ({
+          personalFormData: { ...state.personalFormData, petPreferences: data },
+          completedForms: { ...state.completedForms, petPreferences: true }
+        })),
 
-  setAboutMe: (data) =>
-    set((state) => ({
-      personalFormData: { ...state.personalFormData, aboutMe: data },
-      completedForms: { ...state.completedForms, aboutMe: true }
-    })),
+      setExperience: (data) =>
+        set((state) => ({
+          personalFormData: { ...state.personalFormData, experience: data },
+          completedForms: { ...state.completedForms, experience: true }
+        })),
 
-  nextForm: () =>
-    set((state) => ({ currentFormIndex: state.currentFormIndex + 1 })),
-  prevForm: () =>
-    set((state) => ({ currentFormIndex: state.currentFormIndex - 1 }))
-}));
+      setHomeInfo: (data) =>
+        set((state) => ({
+          personalFormData: { ...state.personalFormData, homeInfo: data },
+          completedForms: { ...state.completedForms, homeInfo: true }
+        })),
+
+      setProfileSetup: (data) =>
+        set((state) => ({
+          personalFormData: { ...state.personalFormData, profileSetup: data },
+          completedForms: { ...state.completedForms, profileSetup: true }
+        })),
+
+      setAboutMe: (data) =>
+        set((state) => ({
+          personalFormData: { ...state.personalFormData, aboutMe: data },
+          completedForms: { ...state.completedForms, aboutMe: true }
+        })),
+
+      nextForm: () =>
+        set((state) => ({ currentFormIndex: state.currentFormIndex + 1 })),
+      prevForm: () =>
+        set((state) => ({ currentFormIndex: state.currentFormIndex - 1 })),
+
+      saveToFirestore: async () => {}
+    }),
+    {
+      name: "personal-storage",
+      getStorage: () => localStorage,
+      onRehydrateStorage: (state) => {
+        console.log("rehydrated state:", state);
+      }
+    }
+  )
+);
 
 export default usePersonalFormStore;
